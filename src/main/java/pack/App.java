26b0b59f41d578069f9008b1afa6197b87dc1259
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-08-07 19:24:29 +0530

    Update App.java

--- src/main/java/pack/App.java
+++ src/main/java/pack/App.java
@@ -1,5 +1,8 @@
 package pack;
 
+import java.io.Reader;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,12 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(str);
+
+
     }
 }
