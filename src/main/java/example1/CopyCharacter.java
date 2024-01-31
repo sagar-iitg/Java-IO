3586a2e0b5bd71fd23a7268f30efc5efccaea951
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-01-31 10:32:35 +0530

    Update CopyCharacter.java

--- src/main/java/example1/CopyCharacter.java
+++ src/main/java/example1/CopyCharacter.java
@@ -1,5 +1,24 @@
 package example1;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CopyCharacter {
-    
+    public static void main(String[] args) throws IOException {
+        FileReader inputstream=null;
+        FileWriter outputstream=null;
+
+        try{
+            inputstream=new FileReader("sample.txt");
+            outputstream=new FileWriter("sample-out1.txt");
+            int c;
+            while((c=inputstream.read())!=-1){
+                outputstream.write(c);
+            }
+        }finally {
+            if(inputstream!=null) inputstream.close();
+            if(outputstream!=null) outputstream.close();
+        }
+    }
 }
